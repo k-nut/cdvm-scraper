d9492d94a736b4aa2f857c3af9a24b552965083c
Knut Hühne <knut@k-nut.eu>
2015-05-24 16:26:48 +0200

    Include type field

--- cdvm/scraper.py
+++ cdvm/scraper.py
@@ -3,46 +3,57 @@
 import re
 import json
 import datetime
-import turbotlib
 from bs4 import BeautifulSoup
 import requests
 
+
+BASE_URL = "http://www.cdvm.gov.ma"
+
 def get_all_pages(base_extension):
-    base_url = "http://www.cdvm.gov.ma"
-    base_extension = "/entreprises-de-marche/societes-de-bourse"
-    base_extension = "/entreprises-de-marche/societe-de-gestion"
-    r = requests.get(base_url + base_extension)
-    text = r.text
-    soup = BeautifulSoup(text)
-    div = soup.find("div", id = re.compile("quicktabs_tabpage_\d_1"))
-    links =div.find_all("li", {"class": "pager-item"}) 
+    div = get_tab_div(base_extension)
+    links = div.find_all("li", {"class": "pager-item"})
     get_links(base_extension)
     for link in links:
         get_links(link.find("a")["href"])
 
+def get_tab_div(base_extension):
+    response = requests.get(BASE_URL + base_extension)
+    text = response.text
+    soup = BeautifulSoup(text)
+    div = soup.find("div", id=re.compile(r"quicktabs_tabpage_\d_1"))
+    return div
 
 def get_links(url_extension):
-    base_url = "http://www.cdvm.gov.ma"
-    r = requests.get(base_url + url_extension)
-    text = r.text
-    soup = BeautifulSoup(text)
-    div = soup.find("div", id = re.compile("quicktabs_tabpage_\d_1"))
+    div = get_tab_div(url_extension)
     tbody = div.find("tbody")
     for tr in tbody.find_all("tr"):
-        extract_data(base_url + tr.find("a")["href"])
+        extract_data(BASE_URL + tr.find("a")["href"])
+
+
+def get_license_type_from_url(url):
+    if "/entreprises-de-marche/societes-de-bourse" in url:
+        return "Sociétés de Bourse"
+    if "/entreprises-de-marche/societes-de-gestion" in url:
+        return "Société de gestion"
+    if "/entreprises-de-marche/teneurs-de-compte" in url:
+        return "Teneurs de comptes"
 
 def extract_data(url):
-    r = requests.get(url)
-    text = r.text
+    response = requests.get(url)
+    text = response.text
     soup = BeautifulSoup(text)
     tbody = soup.find("tbody")
     information = {}
     information["source_url"] = url
     information["sample_date"] = str(datetime.datetime.now())
+    information["type"] = get_license_type_from_url(url)
     trs = tbody.find_all("tr", recursive=False)
     for tr in trs:
         if len(tr) == 5:
-            information[tr.find_all("td")[0].text] = tr.find_all("td")[1].text
+            value = tr.find_all("td")[1].text
+            #There are links to some lists but they are not of any interest to us
+            if not value.startswith("Liste des"):
+                information[tr.find_all("td")[0].text] = value
         else:
             owners = {}
             inner_table = tr.find("table")
@@ -59,7 +70,7 @@ def extract_data(url):
 
             information["Actionnaires"] = owners
 
-    print(json.dumps(information))
+    print json.dumps(information)
 
 if __name__ == "__main__":
     get_all_pages("/entreprises-de-marche/societes-de-bourse")
